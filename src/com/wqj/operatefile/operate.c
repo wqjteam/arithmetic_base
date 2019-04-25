a9e00ef1f288ac2928181a2a76e0c3ce03c84f81
githubwqj2 <2812930196@qq.com>
2019-04-25 11:25:28 +0800

    提交

--- src/com/wqj/operatefile/operate.c
+++ src/com/wqj/operatefile/operate.c
@@ -2,6 +2,47 @@
 // Created by lenovo on 2019/4/23.
 //
 #include <stdio.h>
-int main(){
+
+#define  NUM 10
+
+int main() {
+
+    FILE *in, *out;
+
+    in = fopen("input.txt", "r");
+    out = fopen("output.txt", "w");
+    /**
+     *
+     * 以下为
+     * 输出
+     * 数据
+     * 到
+     * 文件中
+     *
+     * */
+    for (int j = 0; j < 10; ++j) {
+        fprintf(out,"%d",j);
+    }
+    fclose(out);
+
+    int scorelist[NUM], i;
+    for (int i = 0; i < NUM; ++i) {
+            fscanf(in,"%d",&scorelist[i]);
+            printf("%d ",scorelist[i]);
+    }
+
+    printf("\n");
+    /**
+     *
+     * 练习
+     * 指针
+     *
+     *
+     * */
+    int x = 100;
+    int *px;
+    px = &x;
+
+    printf("int的结果为%d",*px);
 
 }
