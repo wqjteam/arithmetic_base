874ba46886320dd192459e8a96f1b7f77ec801bf
githubwqj2 <2812930196@qq.com>
2019-04-25 17:46:18 +0800

    提交

--- src/com/wqj/pointer/operatePointer.c
+++ src/com/wqj/pointer/operatePointer.c
@@ -7,12 +7,17 @@ void swap(int *a, int *b, int len);
 
 void joinstr(int *a, int *b, int len, int *c);
 
+void orderArray(int *a, int len);
+
+void twodimension(char *t[6]);
+
 void main() {
-    int a[] = {1, 2, 3, 4, 5};
+    int a[] = {2, 1, 4, 3, 5};
     int b[] = {6, 7, 8, 9, 10};
-    int c[10] = {};
+    int c[10];
+
     /**
-     * 鏁扮粍鐨勫悕瀛楀氨鏄笅鏍囦负0鐨勫湴鍧�
+     * 数组的名字就是下标为0的地址
      * */
     swap(a, b, sizeof(a) / sizeof(int));
     for (int i = 0; i < sizeof(a) / sizeof(int); ++i) {
@@ -28,19 +33,42 @@ void main() {
 
 
     /**
-     * 娴嬭瘯缁勫悎鏁扮粍
+     * 测试组合数组
      * */
-    joinstr(a,b,sizeof(b) / sizeof(int),c);
+    joinstr(a, b, sizeof(b) / sizeof(int), c);
     for (int i = 0; i < sizeof(c) / sizeof(int); ++i) {
         printf("%d\t", c[i]);
 
     }
     printf("\n");
 
+
+    /**
+     *
+     * 组合排序
+     * */
+    orderArray(a, sizeof(a) / sizeof(int));
+    for (int i = 0; i < sizeof(a) / sizeof(int); ++i) {
+        printf("%d\t", *(a + i));
+    }
+    printf("\n");
+
+
+    /**
+     *
+     * 使用二维数组
+     * */
+    int *f[3][4] = { {0, 1, 2, 3}, {4, 5, 6, 7}, {8, 9, 10, 11} };
+    int (*z)[4]=f;
+    printf("输出:%d\n", *f);
+    printf("输出:%c", *(*(z+1)+2));
+//    twodimension(z);
+    printf("\n");
+
 }
 
 /**
- * 浜掔浉浜ゆ崲鏁版嵁
+ * 互相交换数据
  * */
 void swap(int *a, int *b, int len) {
     for (int i = 0; i < len; ++i) {
@@ -53,7 +81,7 @@ void swap(int *a, int *b, int len) {
 }
 
 /**
-* 缁勫悎鏁版嵁
+* 组合数据
 * */
 void joinstr(int *a, int *b, int len, int *c) {
     for (int i = 0; i < len; ++i) {
@@ -64,4 +92,45 @@ void joinstr(int *a, int *b, int len, int *c) {
         b++;
         c++;
     }
+}
+
+
+void orderArray(int *a, int len) {
+    for (int i = len - 1; i >= 0; i--) {
+        int *max;
+        max = a;
+        /**
+         * 寻找最大的数据
+         * */
+        for (int j = 1; j <= i; ++j) {
+            int *next;
+            next = a + j;
+            if (*max < *next) {
+                max = next;
+            }
+        }
+
+        /**
+         * 交换顺序
+         * */
+        int temp = *(a + i);
+        *(a + i) = *max;
+        *max = temp;
+    }
+}
+
+
+void twodimension(char *t[6]) {
+    for (int i = 0; i < 6; ++i) {
+        int width = sizeof(*(t + i)) / sizeof(int);
+        for (int j = 0; j < width; ++j) {
+            /**
+             * 注意此获取方式
+             * */
+            printf("%c", *(*(t + i)) + j);
+        }
+        printf("\n");
+    }
+
+
 }
