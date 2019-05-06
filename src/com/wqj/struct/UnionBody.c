d37bc4b87032e3960ae5cb4940a9a3c69d59e376
githubwqj2 <2812930196@qq.com>
2019-05-06 14:33:04 +0800

    提交

---
+++ src/com/wqj/struct/UnionBody.c
@@ -0,0 +1,60 @@
+/**
+  * Created by wqj on 2019/5/5.
+ */
+#include <stdio.h>
+#include <process.h>
+#include <mem.h>
+
+/**
+ * 共用体类似于
+ * 全局静态公共变量
+ * 共用体里面的东西只有一个变量可以生效
+ *
+ * */
+union constantV {
+    int age;
+    int sex;
+    char name[20];
+    float grade;
+} un1 = {.name="qwe"};
+
+union MyUnion {
+    int i;
+    char chs[11];
+} u = {10}, u2 = {.chs="niko"};
+
+
+/**
+ *
+ * 在声明一个共用体变量的同时，可以完成其初始化，与结构变量的初始化不同的是，
+ * 只能对共用体变量列表中的一个变量进行初始化
+ *
+ * */
+int main() {
+    printf("结构体长度\n", sizeof(un1));
+    printf("int的长度\n", sizeof(int));
+    printf("un1第一次的值:age:%d,sex:%d,name:%s,grade:%f\n", un1.age, un1.sex, un1.name, un1.grade);
+    un1.age=1;
+    printf("un1第二次的值:age:%d,sex:%d,name:%s,grade:%f\n", un1.age, un1.sex, un1.name, un1.grade);
+    union constantV un2 = {.grade= 4};
+    printf("un2第一次的值:age:%d,sex:%d,name:%s,grade:%f\n", un2.age, un2.sex, un2.name, un2.grade);
+    strcpy(un2.name,"213");
+    printf("un2第二次的值:age:%d,sex:%d,name:%s,grade:%f\n", un2.age, un2.sex, un2.name, un2.grade);
+
+    printf("\n\n\n");
+    union MyUnion mu = {1};  // 默认初始化第一个成员
+    union MyUnion mu2 = {.chs = "hello"};  // 指定要初始化的成员
+    printf("mu.i=%d,mu.chs=%s\n", mu.i, mu.chs);  // 一个成员最后被赋值，其他的会出现垃圾值
+    printf("mu2.i=%d, mu2.chs=%s\n", mu2.i, mu2.chs);
+
+    u2 = u; // 赋值初始化
+    strcpy(u2.chs, "hello world");  // 给共用体变量成员赋值
+    printf("u2.i=%d,u2.chs=%s\n", u2.i, u2.chs);
+    printf("MyUnion = %d\n", sizeof(union MyUnion));
+
+    union MyUnion *up = &u;  //定义指向共用体的指针
+    up->i = 100;
+    printf("%d\n", up->i);
+//    system("pause");
+
+}
--- src/com/wqj/struct/date.c
+++ src/com/wqj/struct/date.c
@@ -2,6 +2,8 @@
   * Created by wqj on 2019/4/30.
  */
 #include <stdio.h>
+#include <malloc.h>
+#include <mem.h>
 
 /**
  * typedef  原类型名  新类型名
@@ -20,11 +22,58 @@ typedef struct {
     birthday by;
 } student;
 
-void main() {
 
+/**
+ * 关于自引用
+ * */
+
+// 错误使用
+//struct MyStruct {
+//    int a;
+//    struct MyStruct my;  // 形成了递归引用
+//};
+
+// 正确使用
+struct MyStruct {
+    int a;
+    struct MyStruct *my;  // 使用指向结构体的指针
+};
+
+typedef struct {
+    int height;
+    char name[20];
+    char sex;
+    int age;
+} person;
+
+
+void main() {
 
     birthday d1 = {2, 11, 2019, 20190210, "张三"};
     student s1 = {20, "asdasdasd", d1};
-    printf("名字%c%c%c%c", s1.by.mon_name[0],s1.by.mon_name[1],s1.by.mon_name[2],s1.by.mon_name[3]);
+    birthday *d1_p = &d1;
+    //成员变量引用方式
+//    1)  结构体变量名.成员名
+//    2)  结构体指针名->成员名；
+//    3) (*结构体指针名).成员名；
+    printf("名字%c%c%c%c\n", s1.by.mon_name[0], s1.by.mon_name[1], s1.by.mon_name[2], s1.by.mon_name[3]);
+    printf("名字%c%c%c%c\n", d1_p->mon_name[0], d1_p->mon_name[1], d1_p->mon_name[2], d1_p->mon_name[3]);
+
+    person per[3] = {{100, "XX", 'M', 18},
+                     {100, "YY", 'F', 19},
+                     {100, "ZZ", 'M', 20}};
+
+    person *perp = (person *) malloc(sizeof(person) * 3);  // 动态开辟内存
+
+    for (int i = 0; i < 3; ++i) {
+        perp[i].height = i;
+        strcpy(perp[i].name, "name");
+        perp[i].sex = i % 2 == 0 ? 'M' : 'F';
+        perp[i].age = i * 10;
+    }
+    /**
+     * 第三行的数据
+     * */
+    printf("年龄:%d\n", (perp + 2)->age);
 }
 
