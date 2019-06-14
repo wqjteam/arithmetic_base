3d9d0ceb32d5eec8146c26e5a3dacbaefa7d8a11
githubwqj2 <2812930196@qq.com>
2019-06-14 18:01:10 +0800

    提交

--- src/com/wqj/ds/tree/OperateTree.c
+++ src/com/wqj/ds/tree/OperateTree.c
@@ -4,12 +4,13 @@
  */
 #include <stdio.h>
 #include <malloc.h>
+#include <math.h>
 #include "OperateTree.h"
 
 
 int main(int argc, char *argv[]) {
 
-    int arr[10] = {1, 2, 3, 4,5,6,7,8};
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8};
 
     int *arr2 = (int *) malloc(sizeof(int) * 10);
     for (int i = 0; i < 10; ++i) {
@@ -17,20 +18,34 @@ int main(int argc, char *argv[]) {
     }
     int arr3[100] = {0};
     BiTree tree = createBinTree(arr, 8);
-    int a=Treedeep(tree);
-    int b=hierarchyNumber(tree, 2,NULL);
-    int c=hierarchyNumber2(tree, 2);
+    int a = Treedeep(tree);
+    int b = hierarchyNumber(tree, 2, NULL);
+    int c = hierarchyNumber2(tree, 2);
 
     /**
      * 测试遍历
      *
      * */
-     printf("前序:\n");
+    printf("\n前序:\n");
     preOrder(tree);
+    printf("\n前序2:\n");
+    preOrder2(tree);
     printf("\n中序:\n");
     inOrder(tree);
+    printf("\n中序2:\n");
+    inOrder2(tree);
     printf("\n后序:\n");
     backOrder(tree);
+    printf("\n后序2:\n");
+    backOrder2(tree);
+
+    printf("\n\n一下为创建广义二叉树表:\n\n\n\n");
+    char generalizedList[] = "1(2(3),4(5(,6),7))@";
+    BiTree generaliz_tree = createGeneralizedTree(generalizedList);
+
+
+    levelOrder(generaliz_tree);
+
 }
 
 
@@ -47,6 +62,7 @@ int initRootTree(BiTree *tree, int value) {
     }
     return 1;
 }
+
 
 /**
  * 从无到有创建一个完全tree
@@ -99,39 +115,39 @@ BiTree createBinTree(int *value, int n) {
    * 遍历出深度
    * */
 int Treedeep(BiTree tree) {
-        if(NULL==tree){
-            return 0;
-        }else{
-            int lh=Treedeep(tree->left);
-            int rh=Treedeep(tree->right);
-            return (lh>rh)?(lh+1):(rh+1);
-        }
+    if (NULL == tree) {
+        return 0;
+    } else {
+        int lh = Treedeep(tree->left);
+        int rh = Treedeep(tree->right);
+        return (lh > rh) ? (lh + 1) : (rh + 1);
+    }
 }
 
 
 int Treedeep2(BiTree tree) {
-    if(NULL==tree){
+    if (NULL == tree) {
         return 0;
-    }else{
-        int lh=Treedeep(tree->left);
-        int rh=Treedeep(tree->right);
-        return (lh>rh)?(lh+1):(rh+1);
+    } else {
+        int lh = Treedeep(tree->left);
+        int rh = Treedeep(tree->right);
+        return (lh > rh) ? (lh + 1) : (rh + 1);
     }
 }
 
-int hierarchyNumber(BiTree tree, int level,int *num) {
-    if(num==NULL){
-        int t=0;
-        num=&t;
+int hierarchyNumber(BiTree tree, int level, int *num) {
+    if (num == NULL) {
+        int t = 0;
+        num = &t;
     }
-    if(tree==NULL){
+    if (tree == NULL) {
         return 0;
     }
-    if(level==1){
+    if (level == 1) {
         (*num)++;
     }
-    hierarchyNumber(tree->right,level-1,num);
-    hierarchyNumber(tree->left,level-1,num);
+    hierarchyNumber(tree->right, level - 1, num);
+    hierarchyNumber(tree->left, level - 1, num);
     return *num;
 }
 
@@ -140,60 +156,171 @@ int hierarchyNumber2(BiTree tree, int level) {
      * 初始化当前节点个数，及下一行节点个数
      * 根节点所在层数为1
      * */
-    int curcount=1,nextcount=0,count=1;
-    LinkList list=NULL;
+    int curcount = 1, nextcount = 0, count = 1;
+    LinkList list = NULL;
     BiTree temp;
-    enqueue(&list,tree);
-    while(list!=NULL){
-        temp=dequeue(&list);
+    enqueue(&list, tree);
+    while (list != NULL) {
+        temp = dequeue(&list);
         curcount--;
-        if(temp->right!=NULL){
-            enqueue(&list,temp->right);
+        if (temp->right != NULL) {
+            enqueue(&list, temp->right);
             nextcount++;
         }
-        if(temp->left!=NULL){
-            enqueue(&list,temp->left);
+        if (temp->left != NULL) {
+            enqueue(&list, temp->left);
             nextcount++;
         }
-        if(curcount==0){
+        if (curcount == 0) {
             count++;
-            if(level==count){
-              return  nextcount;
+            if (level == count) {
+                return nextcount;
             }
             /**
              * 记录下一层需要
              * 先遍历的数据
              * */
-            curcount=nextcount;
-            nextcount=0;
+            curcount = nextcount;
+            nextcount = 0;
         }
     }
     return 0;
 }
 
-int enqueue(LinkList *list,BiTree value){
-    LinkList temp2=(LinkList)malloc(sizeof(LinkNode));
-    temp2->next=NULL;
-    temp2->data=value;
-    if(*list==NULL){
-        *list=temp2;
-    }else{
+/**
+ * 计算非空二叉树(顺序存储) 深度为h
+ * 叶子结点的数量
+ * */
+int leaf1(BiTree *tree, int h) {
+    int count = 0;
+    /**
+     * pow(2,h)-1 代表有
+     * 深度为h,那么他的树节点个数
+     * 2的吃方-1
+     * */
+    for (int i = 0; i < pow(2, h) - 1; ++i) {
+        if (tree[i] != NULL && (tree[i])->left == NULL && tree[i]->right == NULL) {
+            count++;
+        }
+    }
+}
+
+/**
+ * 使用递归算法
+ * */
+int leaf2(BiTree tree) {
+    int count = 0;
+    /**
+     * pow(2,h)-1 代表有
+     * 深度为h,那么他的树节点个数
+     * 2的吃方-1
+     * */
+    if (tree == NULL)
+
+        if (tree->data != -1 && tree->left == NULL && tree->right == NULL) {
+            return 1;
+        } else {
+            return leaf2(tree->right) + leaf2(tree->left);
+        }
+
+}
+
+
+/**
+ * 使用广义表
+ * 构建二叉树
+ * **/
+BiTree createGeneralizedTree(char *tree) {
+
+    BiTree T = NULL, pre, temp;
+    LinkList stack = NULL;
+    int flag = 0;
+    while (*tree != '@') {
+        switch (*tree) {
+            case '(':
+                /**
+                 * 入栈
+                 * */
+                pushSTACK(&stack, pre);
+                flag = 0;
+                break;
+            case ')':
+                popSTACK(&stack);
+                break;
+            case ',':
+                flag = 1;
+                break;
+            default:
+                /**
+                 * 判断根节点
+                 * 是否存在
+                 * */
+                temp = (BiTree) malloc(sizeof(BiNode));
+                temp->data = *tree-'0';
+                temp->left = NULL;
+                temp->right = NULL;
+                pre = temp;
+                if (T == NULL) {
+                    T = temp;
+                    stack = (LinkList) malloc(sizeof(LinkNode));
+                    pushSTACK(&stack, pre);
+                } else {
+                    if (flag) {
+                        readSTACK(stack)->right = temp;
+                    } else {
+                        readSTACK(stack)->left = temp;
+                    }
+                }
+                break;
+
+        }
+
+        tree++;
+    }
+    return T;
+}
+
+
+int enqueue(LinkList *list, BiTree value) {
+    LinkList temp2 = (LinkList) malloc(sizeof(LinkNode));
+    temp2->next = NULL;
+    temp2->data = value;
+    if (*list == NULL) {
+        *list = temp2;
+    } else {
         /**
          * 循环渠道最后一个节点
          * */
         LinkList temp;
-        temp=*list;
-     while(temp->next!=NULL){
-         temp=temp->next;
-     }
-        temp->next=temp2;
+        temp = *list;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = temp2;
     }
 }
 
-BiTree dequeue(LinkList *list){
-    BiTree temp=(*list)->data;
-    (*list)=(*list)->next;
+BiTree dequeue(LinkList *list) {
+    BiTree temp = (*list)->data;
+    (*list) = (*list)->next;
     return temp;
 }
 
+void pushSTACK(LinkList *stack, BiTree treeNode) {
+    LinkList temp = (LinkList) malloc(sizeof(LinkNode));
+    temp->data = treeNode;
+    temp->next = (*stack);
+    *stack = temp;
+}
 
+
+BiTree popSTACK(LinkList *stack) {
+    BiTree tree = (*stack)->data;
+    (*stack) = (*stack)->next;
+    return tree;
+}
+
+BiTree readSTACK(LinkList stack) {
+    BiTree tree = stack->data;
+    return tree;
+}
--- src/com/wqj/ds/tree/TraverseTree.c
+++ src/com/wqj/ds/tree/TraverseTree.c
@@ -4,6 +4,7 @@
  */
 #include <stdio.h>
 #include <OperateTree.h>
+
 /**
  * 先序遍历：
         （1）先访问根节点。
@@ -19,46 +20,178 @@
         （3）最后访问根节点
 
  * */
-void preOrder(BiTree tree){
-    if(tree!=NULL){
-        printf("%d,",tree->data);
+void preOrder(BiTree tree) {
+    if (tree != NULL) {
+        printf("%d,", tree->data);
         preOrder(tree->left);
         preOrder(tree->right);
     }
 
 }
 
-void inOrder(BiTree tree){
-    if(tree!=NULL){
-    inOrder(tree->left);
-    printf("%d,",tree->data);
-    inOrder(tree->right);
+void inOrder(BiTree tree) {
+    if (tree != NULL) {
+        inOrder(tree->left);
+        printf("%d,", tree->data);
+        inOrder(tree->right);
     }
 }
 
-void backOrder(BiTree tree){
-if(tree!=NULL){
-    backOrder(tree->left);
-    backOrder(tree->right);
-    printf("%d,",tree->data);
-}
-}
-
-int preOrder2(BiTree tree){
-    printf("%d,",tree->data);
-    preOrder(tree->left);
-    preOrder(tree->right);
+void backOrder(BiTree tree) {
+    if (tree != NULL) {
+        backOrder(tree->left);
+        backOrder(tree->right);
+        printf("%d,", tree->data);
+    }
 }
 
-int inOrder2(BiTree tree){
-    preOrder(tree->left);
-    printf("%d,",tree->data);
-    preOrder(tree->right);
+
+void levelOrder(BiTree tree) {
+    LinkList queue = NULL;
+    BiTree temp = NULL;
+    enqueue(&queue, tree);
+    int num = 1, level = 1, nextlevelnum = 0;
+    while (num != 0) {
+        temp = dequeue(&queue);
+        printf("(层数:%d:数据:%d\n)", level, temp->data);
+        if (temp->left != NULL) {
+            enqueue(&queue, temp->left);
+            nextlevelnum++;
+        }
+        if (temp->right != NULL) {
+            enqueue(&queue, temp->right);
+            nextlevelnum++;
+        }
+        num--;
+        if (num == 0 && nextlevelnum != 0) {
+            level++;
+            num = nextlevelnum;
+            nextlevelnum = 0;
+        }
+    }
 }
 
-int backOrder2(BiTree tree){
-    preOrder(tree->left);
-    preOrder(tree->left);
-    printf("%d,",tree->data);
+/**
+ *将连接节点重置到根节点
+ * 访问当前节点(同时入栈)
+ * 并取出左节点left 1. 不为空 : 将链接节点重置到left上(继续走while循环)
+ *                  2. 为空   : 弹栈节点temp,将链接节点重置到temp的right上(继续走上面的循环)
+ * */
+void preOrder2(BiTree tree) {
+    LinkList stack = NULL;
+    BiTree temp = NULL;
+    int top = 0;
+    while (tree != NULL || top > 0) {
+        while (tree != NULL) {
+            printf("%d,", tree->data);
+            pushSTACK(&stack, tree);
+            top++;
+            tree = tree->left;
+        }
+        if (top > 0) {
+            /**
+             * 弹栈
+             * */
+            temp = popSTACK(&stack);
+            top--;
+            tree = temp->right;
+        }
+    }
+
+}
+
+
+/**
+ *
+ *从二叉树根节点开始,将根节点入栈
+ * 将指向节点关联到根节点
+ * 访问左节点 1 如过不为空 : 将左节点加入栈中(一直循环到最后一个)
+ *            2 如果为空   : 弹栈将输出,并连接节点指向右节点(继续循环寻找左节点)
+ *直至栈中无节点
+ * */
+void inOrder2(BiTree tree) {
+    LinkList stack = NULL;
+    BiTree temp = NULL;
+    int top = 0;
+    while (top > 0 || tree != NULL) {
+        while (tree != NULL) {
+            pushSTACK(&stack, tree);
+            top++;
+            tree = tree->left;
+        }
+        if (top > 0) {
+            temp = popSTACK(&stack);
+            top--;
+            printf("%d,", temp->data);
+            tree = temp->right;
+        }
+    }
+
+}
+
+
+/**
+ * 后序遍历要求左右子树均完成后才对自身进行访问
+ * 先将右子树（若存在）入栈再将左子树（若存在）入栈，然后指向左孩子（若存在左孩子，否则指向右孩子）
+ * 在对这个部分结点构成的栈进行出栈操作时,应该注意到两个特点。
+ * 第一、叶结点，他是左右子树均为空，而跳出循环时，从栈顶推出。
+ * 第二、任一结点出栈后，考察他和栈顶结点的关系，若是双亲关系，则说明下一个结点在该结点访问完后也可以访问，直接出栈即可。
+ * 若不是双亲关系，则下一个结点应进入本段开头所说的循环将有关结点持续入栈
+ *
+ * */
+void backOrder2(BiTree tree) {
+    LinkList stack = NULL;
+    /**
+     * preTree用于判断是否弹出
+     * 根节点
+     * */
+    BiTree temp = NULL, preTree;
+    /**
+   * 用来标志是是
+   * 左子树还是右子树
+   * 0:左子树
+   * 1:右子数
+   * */
+    int top = 0, flag;
+    while (top > 0 || tree != NULL) {
+        /**
+         * 一直取左子树压栈
+         * */
+        while (tree != NULL) {
+            pushSTACK(&stack, tree);
+            top++;
+            tree = tree->left;
+        }
+        preTree = NULL;
+        flag = 0;
+        while (top > 0 && flag == 0) {
+            temp = readSTACK(stack);
+            /**
+             * 第一次判断是否有右节点
+             * 第二次判断是否等于上次又节点
+             * 如果等于右节点,肯定是根节点
+             * */
+            if (temp->right == preTree) {
+                /**
+                 * 读取stack中的数据,
+                 * 打印
+                 * */
+                printf("%d,", temp->data);
+                popSTACK(&stack);
+                top--;
+                preTree = temp;
+            } else {
+                /**
+                 * 如果有右节点
+                 * 继续将节点压栈,寻找左节点
+                 * */
+                tree = temp->right;
+                /**
+                 * 证明是右子数
+                 * */
+                flag = 1;
+            }
+        }
+    }
 }
 
