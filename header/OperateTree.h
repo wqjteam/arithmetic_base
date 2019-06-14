90b487c54179002d9f81a851d5a9ed68d09caeea
githubwqj2 <2812930196@qq.com>
2019-06-14 18:01:53 +0800

    提交

--- header/OperateTree.h
+++ header/OperateTree.h
@@ -8,16 +8,21 @@
 //
 //#endif //ARITHMETIC_BASE_OPERATETREE_H
 # include<stdio.h>
+
 typedef struct TreeNode {
     int data;
     struct TreeNode *left;
     struct TreeNode *right;
 } BiNode, *BiTree;
 
+/**
+ * 用于队列和栈
+ * */
 typedef struct TreeNodeLocal {
     BiTree data;
     struct TreeNodeLocal *next;
-} LinkNode,*LinkList;
+} LinkNode, *LinkList;
+
 
 int Treedeep(BiTree tree);
 
@@ -25,16 +30,43 @@ int initRootTree(BiTree *tree, int value);
 
 BiTree createBinTree(int *value, int n);
 
-int hierarchyNumber(BiTree tree, int level,int *num);
+int hierarchyNumber(BiTree tree, int level, int *num);
 
 int hierarchyNumber2(BiTree tree, int level);
 
-int enqueue(LinkList *list,BiTree value);
+BiTree createGeneralizedTree(char *tree);
 
-BiTree dequeue(LinkList *list);
+
 
 void preOrder(BiTree tree);
 
 void inOrder(BiTree tree);
 
 void backOrder(BiTree tree);
+
+void levelOrder(BiTree tree);
+
+void preOrder2(BiTree tree);
+
+void inOrder2(BiTree tree);
+
+void backOrder2(BiTree tree);
+
+
+
+
+
+
+
+
+
+
+        int enqueue(LinkList *list, BiTree value);
+
+BiTree dequeue(LinkList *list);
+
+void pushSTACK(LinkList *stack, BiTree treeNode);
+
+BiTree popSTACK(LinkList *stack);
+
+BiTree readSTACK(LinkList stack);
