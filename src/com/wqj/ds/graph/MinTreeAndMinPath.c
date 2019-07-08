83312260b5a46ebb4a876feea1a0dd9df9e70030
githubwqj2 <2812930196@qq.com>
2019-07-08 15:01:09 +0800

    提交

--- src/com/wqj/ds/graph/MinTreeAndMinPath.c
+++ src/com/wqj/ds/graph/MinTreeAndMinPath.c
@@ -3,6 +3,7 @@
   * description:
  */
 #include <OperateGraph.h>
+#include <malloc.h>
 
 
 /**
@@ -91,29 +92,96 @@ int WEARCH_VCR(int s, int dist[], int u) {
 }
 
 
-void TOPO_SORT(Vlink G[],int n,int data[]){
+void TOPO_SORT(Vlink G[], int n, int data[]) {
 
     Elink *p;
-    int j,top=-1;
+    int j, top = -1;
     /**
      * 堆栈初始化
      * */
-    for(int i=0;i<n;i++){
-        if(G[i].indegree==0){
-            G[i].indegree=top;
-            top=i;
+    for (int i = 0; i < n; i++) {
+        if (G[i].indegree == 0) {
+            G[i].indegree = top;
+            top = i;
         }
     }
     /**
      * 依次将入度为0的顶点压榨堆栈中
      * */
     for (int k = 0; k < n; ++k) {
-        if(top==-1){
+        if (top == -1) {
             printf("\n网络中存在回路");
-        }else{
-            j=top;
-            top=G[top].indegree;
-//            V[i]
+        } else {
+            j = top;
+            top = G[top].indegree; /** 退出栈顶元素*/
+            printf("次序i%d:%d", i, G[j].data);   /**输出一个顶底*/
+            p = G[j].link;
+            while (p != NULL) {
+                k = p->adjvex;          /** "删除"一条有j出发的临接顶点*/
+                G[k].indegree--;        /** 当前输出邻接点的入度减一*/
+                if (G[k].indegree == 0) {   /** 新的入度为0的顶点进栈*/
+                    G[k].indegree = top;
+                    top = k;
+                }
+                p = p->next;        /** 找到下一个邻接点*/
+            }
         }
     }
 }
+
+
+/**
+ * 计算三元矩阵的个节点的度
+ * */
+int DEG(int TA[][3], int n, int e, int degree[]) {
+
+    /**
+     * 初始化degree
+     * */
+    for (int i = 0; i < n; i++) {
+        degree[i] = 0;
+    }
+    for (int i = 1; i <= n; i++) {
+        degree[TA[i][0] - 1]++;
+        degree[TA[i][1] - 1]++;
+    }
+}
+
+
+/**
+ * 由邻接矩阵转换为邻接链表
+ * */
+void ADDJLIST(Vlink G[], int n, int data[], int A[][n]) {
+
+    Vlink *deliver = NULL, *new_vlink = NULL;
+    for (int i = 0; i < n; ++i) {
+        /**
+         * 初始化V
+         * */
+        G[i].data = data[i];
+        G[i].link = NULL;
+        for (int j = 0; j < n; ++j) {
+            if (A[i][j] != 0) {
+                new_vlink = (Vlink *) malloc(sizeof(Vlink));
+                new_vlink->data = j;
+                new_vlink->link = NULL;
+                /**
+                 * 将连接添加到V的后面
+                 * */
+                if (G[i].link != NULL) {
+                    G[i].link = new_vlink;
+                } else {
+                    deliver->link = new_vlink;
+                }
+                /**
+                 * 重置deliver到新建的new_vlink上
+                 * */
+                deliver = new_vlink;
+            }
+        }
+    }
+}
+
+
+
+
