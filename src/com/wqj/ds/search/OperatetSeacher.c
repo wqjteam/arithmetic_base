7536c88d8c7e58b55d276854fd45dd1ca5e8fc21
githubwqj2 <2812930196@qq.com>
2019-07-10 18:03:31 +0800

    提交

--- src/com/wqj/ds/search/OperatetSeacher.c
+++ src/com/wqj/ds/search/OperatetSeacher.c
@@ -4,6 +4,10 @@
  */
 #include <stdio.h>
 
+BIN_SEACHER(int resources[], int n, int find_data);
+
+int BIN_SEACHER2(int resources[], int hjgh, int low, int find_data);
+
 int main() {
     printf("123123");
 }
@@ -46,4 +50,28 @@ int BIN_SEACHER(int resources[], int n, int find_data) {
         }
     }
     return -1;
+}
+
+/**
+ * 二分法查找递归
+ * resources 源文件
+ * n 文件长度
+ * find_data寻找的文件
+ * */
+int BIN_SEACHER2(int resources[], int high, int low, int find_data) {
+    int mid;
+    if (low <= high) {
+        mid = (low + high) / 2;
+        if (resources[mid] == find_data) {
+            return mid;
+        }
+        if (find_data > resources[mid]) {
+            return BIN_SEACHER2(resources, high, mid + 1, find_data);
+        }
+        if (find_data < resources[mid]) {
+            return BIN_SEACHER2(resources, mid + 1, low, find_data);
+        }
+    } else {
+        return -1;
+    }
 }
