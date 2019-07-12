e7c6c6095507dad8ce95f11eac2d495b12f26a60
githubwqj2 <2812930196@qq.com>
2019-07-12 17:29:57 +0800

    提交

---
+++ src/com/wqj/ds/innersort/OperateSort.c
@@ -0,0 +1,89 @@
+/**
+  * Created by wqj on 2019/7/11.
+  * description:
+ */
+#include <stdio.h>
+
+void INSERTSORT(int k[], int n);
+
+void BIN_INSERTSORT(int k[], int n);
+
+int main(int argc, char *argv[]) {
+
+
+    int k[] = {49, 38, 65, 97, 76, 13, 27, 79};
+    INSERTSORT(k, 8);
+    for (int i = 0; i < 8; ++i) {
+        printf(" %d", k[i]);
+    }
+
+    printf("\n\n\n");
+    int k2[] = {49, 38, 65, 97, 76, 13, 27, 79};
+    BIN_INSERTSORT(k2, 8);
+    for (int i = 0; i < 8; ++i) {
+        printf(" %d", k2[i]);
+    }
+    return 0;
+}
+
+/**
+ * 插入排序
+ * */
+void INSERTSORT(int k[], int n) {
+    int temp;
+    /**
+     * 需要排序的是2到n 下标就是1到n-1
+     * */
+    for (int i = 1; i < n; ++i) {
+        temp = k[i];
+        int j = i - 1;
+        /**
+         * 移动大于temp的数据
+         * */
+        while (j >= 0 && temp < k[j]) {
+            k[j + 1] = k[j];
+            j--;
+        }
+        /**
+         * 落地temp数据
+         * */
+        k[j + 1] = temp;
+    }
+}
+
+/**
+ * 折半
+ * 插入排序
+ * */
+void BIN_INSERTSORT(int k[], int n) {
+    int low, high, mid, temp;
+    for (int i = 1; i < n; ++i) {
+        temp = k[i];
+        low = 0;
+        high = i - 1;
+        /**
+         * 寻找合适的位置
+         * */
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (temp < k[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        /**
+         * 移动数据
+         * */
+        for (int j = i - 1; j >= low; j--) {
+            k[j + 1] = k[j];
+        }
+        /**
+        * 落地temp数据
+        * */
+        k[low] = temp;
+    }
+}
+
+
